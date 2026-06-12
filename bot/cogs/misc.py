5317853680962ffdf199ed18cb8d22de0574f5ef
Ryuga <vaishnav2353@gmail.com>
2026-06-12 07:48:04 +0530

    chore: minor improvements + refactor

--- bot/cogs/misc.py
+++ bot/cogs/misc.py
@@ -11,21 +11,113 @@ from bot.utils.embed_handler import info
 from bot.constants import embed_space
 
 
-class TagDropdown(discord.ui.Select):
-    def __init__(self, bot, cog_instance):
+class Miscellaneous(commands.Cog):
+    def __init__(self, bot):
         self.bot = bot
-        self.cog_instance = cog_instance
-        options = [
-            discord.SelectOption(label="Ask", value="ask", description="Don't ask to ask, just ask."),
-            discord.SelectOption(label="Markdown", value="markdown", description="Show how to format code using markdown."),
-            discord.SelectOption(label="Run Help", value="run_help", description="Show how to run code with the execution bot."),
-            discord.SelectOption(label="Zen", value="zen", description="The Zen of Python, by Tim Peters."),
-            discord.SelectOption(label="Add to Issues", value="add_to_issues", description="ADD TO ISSUES banner.")
-        ]
-        super().__init__(placeholder="Choose a tag to display...", min_values=1, max_values=1, options=options)
+        self.process = psutil.Process(os.getpid())
+        self.countdown_started = False
 
-    async def callback(self, interaction: discord.Interaction):
-        value = self.values[0]
+    @app_commands.command(name="members")
+    async def members(self, interaction: discord.Interaction):
+        await interaction.response.send_message(
+            embed=info(f"**Member count:** {interaction.guild.member_count}", interaction.guild.me, "")
+        )
+
+    @app_commands.command(name="ping")
+    async def ping(self, interaction: discord.Interaction):
+        start = time.perf_counter()
+        await interaction.response.send_message(embed=info("Pong!", interaction.guild.me))
+        end = time.perf_counter()
+        duration = (end - start) * 1000
+        await interaction.edit_original_response(
+            embed=info(f":ping_pong: {duration:.2f}ms", interaction.guild.me, "Pong!")
+        )
+
+    @app_commands.command(name="stats")
+    @app_commands.checks.cooldown(1, 10)
+    async def stats(self, interaction: discord.Interaction):
+        bot_ram_usage = self.process.memory_full_info().rss / 1024 ** 2
+        bot_ram_usage = f"{bot_ram_usage:.2f} MB"
+        bot_ram_usage_field = self.construct_load_bar_string(
+            self.process.memory_percent(), bot_ram_usage
+        )
+
+        virtual_memory = psutil.virtual_memory()
+        server_ram_usage = f"{virtual_memory.used/1024/1024:.0f} MB"
+        server_ram_usage_field = self.construct_load_bar_string(
+            virtual_memory.percent, server_ram_usage
+        )
+
+        cpu_count = psutil.cpu_count()
+
+        bot_cpu_usage = self.process.cpu_percent()
+        if bot_cpu_usage > 100:
+            bot_cpu_usage = bot_cpu_usage / cpu_count
+        bot_cpu_usage_field = self.construct_load_bar_string(bot_cpu_usage)
+
+        server_cpu_usage = psutil.cpu_percent()
+        if server_cpu_usage > 100:
+            server_cpu_usage = server_cpu_usage / cpu_count
+        server_cpu_usage_field = self.construct_load_bar_string(server_cpu_usage)
+
+        io_counters = self.process.io_counters()
+        io_read_bytes = f"{io_counters.read_bytes/1024/1024:.3f}MB"
+        io_write_bytes = f"{io_counters.write_bytes/1024/1024:.3f}MB"
+
+        field_content = (
+            f"**Bot RAM usage:**{embed_space*7}{bot_ram_usage_field}\n"
+            f"**Server RAM usage:**{embed_space}{server_ram_usage_field}\n"
+            f"**Bot CPU usage:**{embed_space*9}{bot_cpu_usage_field}\n"
+            f"**Server CPU usage:**{embed_space*3}{server_cpu_usage_field}\n"
+            f"**IO (r/w):** {io_read_bytes} / {io_write_bytes}\n"
+        )
+
+        embed = info("", interaction.guild.me, title="")
+        embed.set_author(name="Tortoise BOT", icon_url=interaction.guild.me.avatar.url)
+        embed.add_field(name="Bot Stats", value=field_content)
+        embed.set_footer(text="Tortoise Community")
+
+        await interaction.response.send_message(embed=embed)
+
+    @staticmethod
+    def construct_load_bar_string(percent: int, suffix_message: str = None, size: int = 10):
+        limiters = "|"
+        element_emtpy = "▱"
+        element_full = "▰"
+        constructed = [limiters]
+
+        if size < 8:
+            size = 8
+        if percent > 100:
+            percent = 100
+
+        progress = int(round(percent / size))
+
+        for _ in range(0, progress):
+            constructed.append(element_full)
+        for _ in range(progress, size):
+            constructed.append(element_emtpy)
+
+        constructed.append(limiters)
+        constructed = "".join(constructed)
+
+        if suffix_message is None:
+            constructed = f"{constructed} {percent:.2f}%"
+        else:
+            constructed = f"{constructed} {suffix_message}"
+
+        return constructed
+
+    @app_commands.command(name="tag", description="Select a community resource tag directly from your chat menu.")
+    @app_commands.choices(name=[
+        app_commands.Choice(name="Ask", value="ask"),
+        app_commands.Choice(name="Markdown", value="markdown"),
+        app_commands.Choice(name="Run Help", value="run_help"),
+        app_commands.Choice(name="Zen", value="zen"),
+        app_commands.Choice(name="Add to Issues", value="add_to_issues")
+    ])
+    async def tag(self, interaction: discord.Interaction, name: app_commands.Choice[str]):
+        value = name.value
 
         if value == "ask":
             content = (
@@ -133,115 +225,6 @@ class TagDropdown(discord.ui.Select):
                 ╚═╝░░╚═╝╚═════╝░╚═════╝░  ░░░╚═╝░░░░╚════╝░  ╚═╝╚═════╝░╚═════╝░░╚═════╝░╚══════╝╚═════╝░            
                 """
             await interaction.response.send_message(f"```\n{msg}```")
-
-
-class DropdownView(discord.ui.View):
-    def __init__(self, bot, cog_instance):
-        super().__init__(timeout=60)
-        self.add_item(TagDropdown(bot, cog_instance))
-
-
-class Miscellaneous(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.process = psutil.Process(os.getpid())
-        self.countdown_started = False
-
-    @app_commands.command(name="members")
-    async def members(self, interaction: discord.Interaction):
-        await interaction.response.send_message(
-            embed=info(f"**Member count:** {interaction.guild.member_count}", interaction.guild.me, "")
-        )
-
-    @app_commands.command(name="ping")
-    async def ping(self, interaction: discord.Interaction):
-        start = time.perf_counter()
-        await interaction.response.send_message(embed=info("Pong!", interaction.guild.me))
-        end = time.perf_counter()
-        duration = (end - start) * 1000
-        await interaction.edit_original_response(
-            embed=info(f":ping_pong: {duration:.2f}ms", interaction.guild.me, "Pong!")
-        )
-
-    @app_commands.command(name="stats")
-    @app_commands.checks.cooldown(1, 10)
-    async def stats(self, interaction: discord.Interaction):
-        bot_ram_usage = self.process.memory_full_info().rss / 1024 ** 2
-        bot_ram_usage = f"{bot_ram_usage:.2f} MB"
-        bot_ram_usage_field = self.construct_load_bar_string(
-            self.process.memory_percent(), bot_ram_usage
-        )
-
-        virtual_memory = psutil.virtual_memory()
-        server_ram_usage = f"{virtual_memory.used/1024/1024:.0f} MB"
-        server_ram_usage_field = self.construct_load_bar_string(
-            virtual_memory.percent, server_ram_usage
-        )
-
-        cpu_count = psutil.cpu_count()
-
-        bot_cpu_usage = self.process.cpu_percent()
-        if bot_cpu_usage > 100:
-            bot_cpu_usage = bot_cpu_usage / cpu_count
-        bot_cpu_usage_field = self.construct_load_bar_string(bot_cpu_usage)
-
-        server_cpu_usage = psutil.cpu_percent()
-        if server_cpu_usage > 100:
-            server_cpu_usage = server_cpu_usage / cpu_count
-        server_cpu_usage_field = self.construct_load_bar_string(server_cpu_usage)
-
-        io_counters = self.process.io_counters()
-        io_read_bytes = f"{io_counters.read_bytes/1024/1024:.3f}MB"
-        io_write_bytes = f"{io_counters.write_bytes/1024/1024:.3f}MB"
-
-        field_content = (
-            f"**Bot RAM usage:**{embed_space*7}{bot_ram_usage_field}\n"
-            f"**Server RAM usage:**{embed_space}{server_ram_usage_field}\n"
-            f"**Bot CPU usage:**{embed_space*9}{bot_cpu_usage_field}\n"
-            f"**Server CPU usage:**{embed_space*3}{server_cpu_usage_field}\n"
-            f"**IO (r/w):** {io_read_bytes} / {io_write_bytes}\n"
-        )
-
-        embed = info("", interaction.guild.me, title="")
-        embed.set_author(name="Tortoise BOT", icon_url=interaction.guild.me.avatar.url)
-        embed.add_field(name="Bot Stats", value=field_content)
-        embed.set_footer(text="Tortoise Community")
-
-        await interaction.response.send_message(embed=embed)
-
-    @staticmethod
-    def construct_load_bar_string(percent: int, suffix_message: str = None, size: int = 10):
-        limiters = "|"
-        element_emtpy = "▱"
-        element_full = "▰"
-        constructed = [limiters]
-
-        if size < 8:
-            size = 8
-        if percent > 100:
-            percent = 100
-
-        progress = int(round(percent / size))
-
-        for _ in range(0, progress):
-            constructed.append(element_full)
-        for _ in range(progress, size):
-            constructed.append(element_emtpy)
-
-        constructed.append(limiters)
-        constructed = "".join(constructed)
-
-        if suffix_message is None:
-            constructed = f"{constructed} {percent:.2f}%"
-        else:
-            constructed = f"{constructed} {suffix_message}"
-
-        return constructed
-
-    @app_commands.command(name="tag", description="Select a community resource tag from the menu")
-    async def tag(self, interaction: discord.Interaction):
-        view = DropdownView(self.bot, self)
-        await interaction.response.send_message("Please select an option below:", view=view, ephemeral=True)
 
 
 async def setup(bot):
